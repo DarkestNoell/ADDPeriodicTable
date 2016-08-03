34e93599fec7599a97de18d5357d842376fb8d9f
David Leiter <davidleiter1@gmail.com>
2016-08-03 08:44:09 -0600

    Creating the Periodic Table

--- MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
+++ MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
@@ -11,27 +11,13 @@ namespace MADDPeriodicTable.Controllers
     {
         public ActionResult Index()
         {
-            Random randy = new Random();
-            PeriodicTableEntities1 pte = new PeriodicTableEntities1();
-            int id = randy.Next(39) + 1;
+            //Random randy = new Random();
+            //PeriodicTableEntities1 pte = new PeriodicTableEntities1();
+            //int id = randy.Next(39) + 1;
 
-            Compound selectedCompound = pte.Compounds.Where(compound => compound.ID == id).First();
-            Console.WriteLine(selectedCompound.Compound_Name);
-            return View(selectedCompound);
-        }
-
-        public ActionResult About()
-        {
-
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            //Compound selectedCompound = pte.Compounds.Where(compound => compound.ID == id).First();
+            //Console.WriteLine(selectedCompound.Compound_Name);
+            return View(/*selectedCompound*/);
         }
     }
 }
