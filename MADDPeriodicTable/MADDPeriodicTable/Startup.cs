1bfd0cba9605aac14613f127049345547d449e0c
DomingosF <Dfernandes@student.neumont.edu>
2016-08-05 10:51:55 -0600

    added files back in

---
+++ MADDPeriodicTable/MADDPeriodicTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MADDPeriodicTable.Startup))]
+namespace MADDPeriodicTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
