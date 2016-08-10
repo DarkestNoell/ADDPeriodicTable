3e40ea39608f23fd2b8df67228425bd58702e31c
DomingosF <Dfernandes@student.neumont.edu>
2016-08-10 11:01:45 -0600

    Email and Contact Us

--- MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
+++ MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MADDPeriodicTable.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace MADDPeriodicTable.Controllers
 {
@@ -31,6 +34,35 @@ namespace MADDPeriodicTable.Controllers
             PeriodicTableEntities1 pte = new PeriodicTableEntities1();
             var data = pte.sp_s_Com().ToList();
             ViewBag.userdetail = data;
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ContactUs(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("dynamicweblab@outlook.com"));
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
             return View();
         }
     }
