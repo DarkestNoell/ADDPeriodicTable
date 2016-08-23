a1e429c81703cdb18172f643ba10d8e9939af557
Austin F <foutyaustinj@gmail.com>
2016-08-23 10:10:09 -0600

    Added a list of videos the user can enjoy for comic relief

--- MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
+++ MADDPeriodicTable/MADDPeriodicTable/Controllers/HomeController.cs
@@ -221,6 +221,7 @@ namespace MADDPeriodicTable.Controllers
 
             return View("Dragging", tuple);
         }
+
         public ActionResult Elements()
         {
             PeriodicTableEntities pte = new PeriodicTableEntities();
@@ -263,6 +264,11 @@ namespace MADDPeriodicTable.Controllers
             return View(model);
         }
         public ActionResult Sent()
+        {
+            return View();
+        }
+
+        public ActionResult Relief()
         {
             return View();
         }
